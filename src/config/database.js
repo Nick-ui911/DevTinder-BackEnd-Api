e3d90e0066d1721deda8b52db5fe421ab4993026
Nick-ui911 <nbaghel392@gmail.com>
2025-01-29 22:37:49 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Nikhil:Nick%40102030@nicknode.qujbj.mongodb.net/"
+      "mongodb+srv://Nikhil:@nicknode.qujbj.mongodb.net/"
     );
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
