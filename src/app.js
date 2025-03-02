77a0270e1da049f04fa8b41faec1860a9995c642
Nick-ui911 <nbaghel392@gmail.com>
2025-03-02 13:50:52 +0530

    5000

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get("/test-cors", cors(corsOptions), (req, res) => {
 connectDB()
   .then(() => {
     console.log("Database is connected");
-    app.listen(3000, () => {
+    app.listen(5000, () => {
       console.log("Server is running on port 3000");
     });
   })
