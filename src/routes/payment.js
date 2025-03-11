8ac959026d38128b401920c761ca6a2bc6b6f1a8
Nick-ui911 <nbaghel392@gmail.com>
2025-03-12 00:24:05 +0530

    .

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -6,6 +6,7 @@ const Payment = require("../models/payment");
 const membershipAmount = require("../utils/constant");
 const User = require("../models/user");
 const { sendEmail } = require("../utils/sendEmail");
+const Razorpay = require("razorpay");
 
 paymentRouter.post("/payment/create", authUser, async (req, res) => {
   try {
@@ -41,56 +42,66 @@ paymentRouter.post("/payment/create", authUser, async (req, res) => {
 });
 paymentRouter.post("/payment/webhook", async (req, res) => {
   try {
-    console.log("✅ Webhook received:", JSON.stringify(req.body, null, 2));
-
     const secret = process.env.RAZORPAY_WEB_HOOK_SECRET_KEY;
     const signature = req.get("x-razorpay-signature");
     const body = JSON.stringify(req.body);
 
     // Validate webhook signature
-    const isValid = instance.validateWebhookSignature(body, signature, secret);
+    const isValid = Razorpay.validateWebhookSignature(body, signature, secret);
     if (!isValid) {
-      console.log("❌ Invalid webhook signature");
       return res.status(400).json({ message: "Invalid webhook signature" });
     }
 
     // Extract payment data
     const paymentData = req.body.payload.payment.entity;
     const event = req.body.event;
-    console.log("✅ Payment Event:", event);
-    console.log("🔹 Payment Data:", paymentData);
 
     // Find and update payment in DB
     const payment = await Payment.findOne({ orderId: paymentData.order_id });
 
     if (!payment) {
-      console.log("❌ Payment not found in DB");
       return res.status(404).json({ message: "Payment not found" });
     }
 
     payment.status = paymentData.status;
     await payment.save();
-    console.log("✅ Payment status updated in DB:", payment.status);
 
     // Find and update user based on payment
     const user = await User.findOne({ _id: payment.userId });
 
     if (!user) {
-      console.log("❌ User not found for payment");
       return res.status(404).json({ message: "User not found" });
     }
 
     user.isPremium = true;
     user.membershipType = payment.notes.membershipType;
     await user.save();
-    console.log("✅ User membership updated in DB:", user.membershipType);
+
+    // Handle Payment Events
+    if (event === "payment.captured") {
+      // here i am currently sending a mail to the ns048019@gmail.com because we are in ses sandbox mode
+      sendEmail(
+        "ns048019@gmail.com",
+        "Payment Status",
+        `Hi ${user.name} your payment of Rs ${paymentData.amount} has been captured successfully`
+      );
+      //   console.log("✅ Payment Successful:", paymentData);
+      // Additional success logic here , like sending success email to user
+    } else if (event === "payment.failed") {
+      sendEmail(
+        "ns048019@gmail.com",
+        "Payment Status",
+        `Hi ${user.name} your payment of Rs ${paymentData.amount} has been failed due to some reason`
+      );
+      //   console.log("❌ Payment Failed:", paymentData.error_reason);
+      // Additional failure logic here , like sending failure email to user
+    }
 
     res.status(200).json({ status: "Webhook received" });
   } catch (error) {
-    console.error("❌ Webhook error:", error.message);
+    console.error("Webhook error:", error.message);
     res.status(500).json({ error: "Webhook processing failed" });
   }
 });
-
 
 module.exports = paymentRouter;
